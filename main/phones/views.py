5d25a2efa482db401682073b502838aed3719632
user <maksim.glinka@gmail.com>
2023-01-09 21:46:18 +0300

    update file views

--- main/phones/views.py
+++ main/phones/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.db.models.functions import Coalesce
 from django.shortcuts import render, redirect
+
+from phones.models import Phone
 
 
 def index(request):
@@ -10,11 +10,30 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+
+    sort = request.GET.get('sort')
+
+    if sort == 'name':
+        phone_objects = Phone.objects.all().order_by('name')
+    elif sort == 'min_price':
+        phone_objects = Phone.objects.all().order_by('price')
+    elif sort == 'max_price':
+        phone_objects = Phone.objects.all().order_by('-price')
+    else:
+        phone_objects = Phone.objects.all()
+
+
+    context = {'phones': phone_objects}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone_object = Phone.objects.filter(slug=slug)
+    for phone in phone_object:
+        context = {'phone': {'name': phone.name, 'price': phone.price, 'release_date': phone.release_date, 'lte_exists': phone.lte_exists, 'image': phone.image} }
+     # print(phone.name)
+
+
+    print(context)
     return render(request, template, context)
